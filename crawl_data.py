a4f98c99449dbc0d5d414dea05fc92fa3d3c6b6f
yun <kilicapeto@gmail.com>
2023-02-20 21:44:31 +0800

    add: proxy

--- crawl_data.py
+++ crawl_data.py
@@ -1,14 +1,16 @@
-import asyncio
-import datetime
+import os
 import json
+
+import asyncio
 import traceback
 import aiohttp
 import urllib.parse
-import os
 
 domain = 'https://www.partslink24.com'
+proxy = 'https://127.0.0.1:8080'
 urls = [
-    ('Jaguar', 'https://www.partslink24.com/p5jlr/extern/vehicle/models?lang=zh-TW&serviceName=jaguar_parts&upds=2023-02-14--09-36&_=1676796282460'),
+    # ('Jaguar', 'https://www.partslink24.com/p5jlr/extern/vehicle/models?lang=zh-TW&serviceName=jaguar_parts&upds=2023-02-14--09-36&_=1676796282460'),
+    ('Land Rover', 'https://www.partslink24.com/p5jlr/extern/vehicle/models?lang=zh-TW&serviceName=landrover_parts&_=1676885404540'),
 ]
 
 
@@ -22,8 +24,8 @@ async def crawl_depth(link, name):
     if not link:
         return
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as response:
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            async with session.get(link, proxy=proxy, ssl=False) as response:
                 # NOTE: check the reponse is json or not.
                 if not response.headers.get('content-type', '').startswith('application/json'):
                     return 
@@ -35,14 +37,16 @@ async def crawl_depth(link, name):
                     component_value = detail.get('values', {})
                     component_name = component_value.get('name', '') or component_value.get('code', '')
                     if child_url and component_name:
-                        await crawl_depth(urllib.parse.urljoin(domain, child_url), os.path.join(name, component_name, component_name))
+                        await crawl_depth(
+                            urllib.parse.urljoin(domain, child_url),
+                            os.path.join(name, component_name, component_name)
+                        )
     except Exception as e:
         print(traceback.format_exc())
 
 
 async def main():
     async with asyncio.TaskGroup() as tg:
-        s = datetime.datetime.now()
         for name, url in urls:
             tg.create_task(crawl_depth(url, f'{name}/{name}'))
 
